700da3e912177559fcbbf84983751744df45757a
Colleen Carroll <leenmcarroll@gmail.com>
2015-05-25 14:41:45 -0400

    added the background

--- user-mgmt.js
+++ user-mgmt.js
@@ -1,31 +1,86 @@
-$ (function (){
-  'use strict';
+$ (function () {
+   'use strict';
+
+  $('.user-input').on('submit', function (e) {
+    e.preventDefault();
+
+    var userEntry = {};
+
+    $('.user-field').each(function() {
+      userEntry[this.name] = this.value;
+    });
+
+    // console.log (userEntry);
+
+    var li = $('<li class="user-field"><span class="user-label"></span><button class="user-remove">X</button></li>');
+    li.find('.user-label').text(userEntry['first-name'] + (" ") + userEntry['last-name'] + (" ") + userEntry['email']);
+    // console.log (txt);
+    // $('list').append($('<li class = "user-field"></li>').text(txt.val()));
+    $('.user-list').append(li);
+
+    //txt.val('');
 
 
 
-  $ ('button').click(function(e) {
-  e.preventDefault();
+    // list.innerHTML = listUsers.map(function (userEntry, i) {
+    //     return '<li class="user-li">' +
+    //       escape(userEntry) +
+    //       '<button type="button" class="remove" data-index="' + i + '">X</button>'+
+    //       '</li>';
+    //   }).join('');
 
-  addUser();
-  redrawUsers();
-
-function addUser() {}
-
+  });
 });
+     //$(this).find('.todo-txt');
 
-
+// var users = [];
+//
+// $ (function (){
+//   'use strict';
+//
+//
+//
+//   $ ('button').click(function(e) {
+//   e.preventDefault();
+//
+//   addUser();
+//   redrawUsers();
+// });
+//
+// function addUser() {
+//   var txt = document.querySelector('.user-input');
+//
+//   if (txt.value) {
+//     users.push(txt.value);
+//     users.sort();
+//     txt.value = '';
+//   }
+// }
+//
+// function redrawUsers() {
+//   var list = document.querySelector('.user-list');
+//
+//   list.innerHTML = users.map(function (user, i) {
+//     return '<li class="user-item">' +
+//       escape(user) +
+//       '<button type="button" class="remove" data-index="' + i + '">X</button>'+
+//       '</li>';
+//   }).join('');
+// }
+//
+// });
 
 // var toValidate = $ ('.userFirstName, .userLastName, .userEmail');
 //
-// toValidate.keyup(function() {
-//   if ($(this).val().length > 0){
+// toValidate.keyup(function(e) {
+//   if ($(e.target).val().length > 0){
 //     $('.add').removeAttr('disabled');
 //   } else{
 //
 //   }
 // });
 
-
+// var txt = document.querySelector('.user-input');
 
 // var textEntered = true;
 // $ ("input[type=text]").each(function () {
@@ -37,5 +92,29 @@ function addUser() {}
 //   }
 // });
 //
-// function addUser() {
-//   var txt = document.querySelector('.user-input');
+// $(document).ready(function() {
+//
+//   var arrList = [];
+//
+//   var firstName = ".firstname";
+//   var lastName = ".lastname";
+//   var email = '.email';
+//
+//   $(".userfields").submit(function (e){
+//
+//   e.preventDefault();
+//
+//   $(".userlist").append($(".firstname, .lastname, "))
+//
+// }
+//
+// }
+//
+//
+//
+// )
+
+// var txt = $(this).find('.todo-txt');
+// var li = $('<li class="todo-item"><span class="todo-label"></span><button class="todo-remove">X</button></li>');
+// li.find('.todo-label').text(txt.val());
+// $('.todo-list').append(li);
